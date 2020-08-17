fdef074612100f65db307ac016f67013b6d5ea03
Bryan Pan <bryanpan342@gmail.com>
2020-08-16 19:59:45 -0700

    mobile friendly?

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
-console.log("imported");
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+var ocean_bubbles = document.getElementsByClassName("ocean-bubbles");
+var light_corals = document.getElementsByClassName("light-coral");
+var dark_corals = document.getElementsByClassName("dark-coral");
+var polypses = document.getElementsByClassName("polyps");
+
+function resize() {
+  console.log('resized');
+  var bubble_width = min(600, screen.width);
+  var coral_width = min(475, screen.width * .74 );
+  var polyps_width = min(250, screen.width * .266 );
+  Array.from(ocean_bubbles).forEach(function (bubble) { 
+    bubble.style.width =`${bubble_width}px`;
+  }); 
+  Array.from(light_corals).forEach(function (coral) {
+    coral.style.width = `${coral_width}px`;
+  }); 
+  Array.from(dark_corals).forEach(function (coral) { 
+    coral.style.width =`${coral_width}px`;
+  }); 
+  Array.from(polypses).forEach(function (polyps) { 
+    polyps.style.width =`${polyps_width}px`;
+  }); 
+}
+resize();
+window.onresize = resize;
