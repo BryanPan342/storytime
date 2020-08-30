0163ad07293efbc0738fa01f59c88ca115e674cc
Bryan Pan <bryanpan342@gmail.com>
2020-08-30 13:43:26 -0700

    doesn't work but a place to start

--- scripts/main.js
+++ scripts/main.js
@@ -2,39 +2,49 @@ function min(a, b) {
   return a < b ? a : b;
 }
 
-var ocean_bubbles = document.getElementsByClassName("ocean-bubbles");
-var light_corals = document.getElementsByClassName("light-coral");
-var dark_corals = document.getElementsByClassName("dark-coral");
-var polypses = document.getElementsByClassName("polyps");
-
-function resize() {
-  var bubble_width = min(600, screen.width);
-  var coral_width = min(475, screen.width * .74 );
-  var polyps_width = min(250, screen.width * .266 );
-
-  Array.from(ocean_bubbles).forEach(function (bubble) { 
-    bubble.style.width =`${bubble_width}px`;
-  }); 
-  Array.from(light_corals).forEach(function (coral) {
-    coral.style.width = `${coral_width}px`;
-  }); 
-  Array.from(dark_corals).forEach(function (coral) { 
-    coral.style.width =`${coral_width}px`;
-  }); 
-  Array.from(polypses).forEach(function (polyps) { 
-    polyps.style.width =`${polyps_width}px`;
-  }); 
+function addDims(element) {
+  maxDimsForIds[element.getAttribute('id')] = { 'width': element.offsetWidth, 'height': element.offsetHeight};
 }
 
-resize();
+function storeDivs() {
+  for (var e = document.getElementsByTagName("div"), t = 0; t < e.length; t++){
+    "ocean-bubbles" === e[t].getAttribute('class') && ocean_bubbles.push(e[t]) && addDims(e[t]),
+    "light-coral" === e[t].getAttribute('class') && light_corals.push(e[t]) && addDims(e[t]),
+    "dark-coral" === e[t].getAttribute('class') && dark_corals.push(e[t]) && addDims(e[t]),
+    "polyps" === e[t].getAttribute('class') && polypses.push(e[t]) && addDims(e[t]),
+    "layer" === e[t].getAttribute('class') && layers.push(e[t]) && addDims(e[t]);
+  }
+}
+
+var maxDimsForIds = new Object;
+var classMultipliers = {
+  'ocean-bubbles': 1,
+  'light-coral': .74,
+  'dark-coral': .74,
+  'polyps': .266,
+};
+var layers = new Array, ocean_bubbles = new Array, light_corals = new Array, dark_corals = new Array, polypses = new Array;
+
+function resize() {
+  console.log('resizing');
+  Object.keys(maxDimsForIds).map((key) => {
+    const e = maxDimsForIds[key];
+    let element = document.getElementById(key);
+    const m = classMultipliers[element.getAttribute('class')];
+    if (m) {
+      console.log(`${min(e['width'], screen.width * m)}px`);
+      element.style.width = `${min(e['width'], screen.width * m)}px`;
+      element.style.height = `${min(e['height'], screen.height * m)}px`;
+    }
+  });
+}
+
+storeDivs(), resize();
 window.onresize = resize;
 
 function handleScroll() {
-  var scrollLeft = window.pageXOffset
-  Array.from(document.getElementsByClassName("parallax")).forEach(function (element) {
-    console.log(scrollLeft); 
-    element.style.transform = `translateX(${scrollLeft*.1}px)`;
-  }); 
+  var scrollLeft = window.pageXOffset;
+  document.getElementById("ocean-background-bubbles").style.left = `${scrollLeft*.5}px`;
 };
 
 window.addEventListener('scroll', handleScroll, { passive: true });
