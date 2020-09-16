80938ee729ee7ecd3fcdbe4abcf6f976323a80d6
Bryan Pan <bryanpan@Bryans-MBP.lan>
2020-09-15 21:25:43 -0700

    add rocks

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ function storeImgs() {
   for (var e = document.getElementsByTagName('img'), t = 0; t < e.length; t++){
     const et = e[t];
     "feesh" === et.getAttribute('class') && (feesh = et) && addDims(et),
+    "rocks" === et.getAttribute('class') && rocks.push(et) && addDims(et),
     "ocean-bubbles" === et.getAttribute('class') && ocean_bubbles.push(et) && addDims(et),
     "light-coral" === et.getAttribute('class') && light_corals.push(et) && addDims(et),
     "dark-coral" === et.getAttribute('class') && dark_corals.push(et) && addDims(et),
@@ -30,12 +31,13 @@ function storeDivs() {
 var maxDimsForIds = new Object;
 var classMultipliers = {
   'feesh': .25,
+  'rocks': .8,
   'ocean-bubbles': 1,
   'light-coral': .54,
   'dark-coral': .54,
   'polyps': .20,
 };
-var layers = new Array, feesh, ocean_bubbles = new Array, light_corals = new Array, dark_corals = new Array, polypses = new Array;
+var feesh, layers = rocks = ocean_bubbles = light_corals = dark_corals = polypses = new Array;
 
 function resize() {
   console.log('resizing');
@@ -46,7 +48,12 @@ function resize() {
     console.log(element);
     const m = classMultipliers[element.getAttribute('class')];
     if (m) {
-      const calc_height = window.innerHeight * m;
+      let calc_height;
+      if(key == 'rocks-1') {
+        calc_height =  window.innerHeight;
+      } else {
+        calc_height = window.innerHeight * m;
+      }
       const calc_width = calc_height * e.widthMultiplier;
       element.style.width = `${min(e.width, calc_width)}px`;
       element.style.height = `${min(e.height, calc_height)}px`;
